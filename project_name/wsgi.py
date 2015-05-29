d11da836a28a02a43352daf9386b669c33933514
Alexei Martchenko <alexei@martchenko.com.br>
2015-05-29 16:35:24 -0300

    NewRelic initialize no WSGI

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 # {% now "Y/m/d H:m" %} - Created
 
-"""
-WSGI config for {{ project_name }} project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
-"""
-
 import os
+
+try:
+    import newrelic.agent
+    newrelic.agent.initialize('/home/dev/newrelic-{{ project_name }}.ini', 'django17')
+except Exception, e:
+    pass
+
 settings_name = "settings.local" if os.name == 'nt' else "settings.remote"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_name)
 
